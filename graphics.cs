0af29277bdcef9e9b280a4bae586aef2da428aff
Osempu <osarosempu@gmail.com>
2026-04-02 22:04:50 -0500

    Added different output formats

--- graphics.cs
+++ graphics.cs
@@ -9,124 +9,12 @@ public class SharpFetchGraphics
     {
         var sysInfo = new Sysinfo();
 
-        var grid = new Grid()
-               .AddColumn(new GridColumn().NoWrap().PadRight(4))
-               .AddColumn()
-               .AddRow("[yellow] CPU[/]", $"[yellow]{sysInfo.CPU}[/]")
-               .AddRow("[magenta] Memory[/]", $"[magenta]{sysInfo.MemoryInfo}[/]")
-               .AddRow("[cyan] GPU[/]", $"[cyan]{sysInfo.GPU}[/]")
-               .AddRow("[red] Uptime[/]", $"[red]{sysInfo.Uptime}[/]")
-               .AddRow("[blue] Window Manager[/]", $"[blue]{sysInfo.WindowManager}[/]")
-               .AddRow("[green] Terminal[/]", $"[green]{sysInfo.Terminal}[/]")
-               .AddRow("[yellow] Shell[/]", $"[yellow]{sysInfo.Shell}[/]")
-               .AddRow("[magenta] BIOS[/]", $"[magenta]{sysInfo.Bios}[/]")
-               .AddRow("[cyan] Date/Time[/]", $"[cyan]{sysInfo.DateTime}[/]")
-               .AddRow("[DarkOliveGreen2] Disk Space[/]", $"[DarkOliveGreen2]{sysInfo.DiskInfo}[/]");
+        ConsoleOutput.PrintAsPanels(sysInfo);
 
-        var systemGrid = new Grid()
-            .AddColumn(new GridColumn().NoWrap().PadRight(4))
-            .AddColumn()
-            .AddRow("[red] OS[/]", $"[red]{Sysinfo.GetOSName()}[/]")
-            .AddRow("[blue] OS Description[/]", $"[blue]{sysInfo.OsDescription}[/]")
-            .AddRow("[yellow] Kernel[/]", $"[yellow]{sysInfo.Kernel}[/]");
+        ConsoleOutput.PrintAsTrees(sysInfo);
 
-        var hardwareGrid = new Grid()
-            .AddColumn(new GridColumn().NoWrap().PadRight(4))
-            .AddColumn()
-            .AddRow("[yellow] CPU[/]", $"[yellow]{sysInfo.CPU}[/]")
-            .AddRow("[magenta] Memory[/]", $"[magenta]{sysInfo.MemoryInfo}[/]")
-            .AddRow("[cyan] GPU[/]", $"[cyan]{sysInfo.GPU}[/]")
-            .AddRow("[magenta] BIOS[/]", $"[magenta]{sysInfo.Bios}[/]");
+        ConsoleOutput.PrintMinmal(sysInfo);
 
-        // Create memory chart
-        var memoryChart = new BreakdownChart()
-            .FullSize()
-            .Width(60)
-            .ShowPercentage()
-            .AddItem("Used", Helpers.ToGBDouble((ulong)sysInfo.MemoryInfo.UsedPhysicalMemoryBytes), Color.Red)
-            .AddItem("Free", Helpers.ToGBDouble((ulong)sysInfo.MemoryInfo.AvailablePhysicalMemoryBytes), Color.Green);
-
-        var gridNoIcons = new Grid()
-            .AddColumn(new GridColumn().NoWrap().PadRight(4))
-            .AddColumn()
-            .AddRow("[red]OS [/]", $"{Sysinfo.GetOSName()}")
-            .AddRow("[blue]OS Description[/]", $"{sysInfo.OsDescription}")
-            .AddRow("[yellow]Kernel[/]", $"{sysInfo.Kernel}")
-            .AddRow("[yellow]CPU[/]", $"{sysInfo.CPU}")
-            .AddRow("[magenta]Memory[/]", $"{sysInfo.MemoryInfo}")
-            .AddRow("[cyan]GPU[/]", $"{sysInfo.GPU}")
-            .AddRow("[red]Uptime[/]", $"{sysInfo.Uptime}")
-            .AddRow("[blue]Window Manager[/]", $"{sysInfo.WindowManager}")
-            .AddRow("[green]Terminal[/]", $"{sysInfo.Terminal}")
-            .AddRow("[yellow]Shell[/]", $"{sysInfo.Shell}")
-            .AddRow("[magenta]BIOS[/]", $"{sysInfo.Bios}")
-            .AddRow("[cyan]Date/Time[/]", $"{sysInfo.DateTime}")
-            .AddRow("[DarkOliveGreen2]Disk Space[/]", $"{sysInfo.DiskInfo}");
-
-        // Display System Info panel
-        AnsiConsole.Write(
-            new Panel(hideIcons ? grid : systemGrid)
-                .Header("System Info", Justify.Center)
-                .RoundedBorder()
-                .BorderColor(Color.Green));
-
-        // Display Hardware Info panel
-        AnsiConsole.Write(
-            new Panel(hideIcons ? grid : hardwareGrid)
-                .Header("Hardware Info", Justify.Center)
-                .RoundedBorder()
-                .BorderColor(Color.Aqua));
-
-        // Display Memory Chart panel
-        Render("Memory Usage", memoryChart);
-
-        if (!isMinimal)
-        {
-            var (free, total) = sysInfo.GetDiskChart();
-
-            // Create disk chart
-            var diskChart = new BreakdownChart()
-                .FullSize()
-                .Width(60)
-                .ShowPercentage()
-                .AddItem("Used", total - free, Color.Red)
-                .AddItem("Free", free, Color.Green);
-
-            Render("Disk Space", diskChart);
-
-            var environmentGrid = new Grid()
-                .AddColumn(new GridColumn().NoWrap().PadRight(4))
-                .AddColumn()
-                .AddRow("[red] Window Manager[/]", $"[red]{sysInfo.WindowManager}[/]")
-                .AddRow("[green] Terminal[/]", $"[green]{sysInfo.Terminal}[/]")
-                .AddRow("[yellow] Shell[/]", $"[yellow]{sysInfo.Shell}[/]");
-
-            var status = new Grid()
-                .AddColumn(new GridColumn().NoWrap().PadRight(4))
-                .AddColumn()
-                .AddRow("[cyan] Date/Time[/]", $"[cyan]{sysInfo.DateTime}[/]")
-                .AddRow("[red] Uptime[/]", $"[red]{sysInfo.Uptime}[/]");
-
-            AnsiConsole.Write(
-            new Panel(hideIcons ? grid : environmentGrid)
-                .Header("Environment Info", Justify.Center)
-                .RoundedBorder()
-                .BorderColor(Color.Blue));
-
-            AnsiConsole.Write(
-                new Panel(hideIcons ? grid : status)
-                    .Header("Status Info", Justify.Center)
-                    .RoundedBorder()
-                    .BorderColor(Color.Pink1));
-        }
+        ConsoleOutput.PrintAsLeftPanel(sysInfo);
     }
-
-    private static void Render(string title, IRenderable chart)
-    {
-        AnsiConsole.Write(
-            new Panel(chart)
-                .Padding(1, 1)
-                .Header(title));
-    }
-
 }
--- pland.md
+++ pland.md
@@ -21,10 +21,17 @@
 - [x] Add a minimal and full flags to display minimal system information or full info.
 - [x] Add Charts widget to show memory and diskspace.
 
-### Day 4 (Next Stream, Friday 27th)
+### Day 4 (⌚ In Progress ... )
+- [ ] Plan and implement a way to let the user decide what to print and from there refactor and make no icons logic work as well as for minimal flag and any other existing option for the cli.
 - [ ] Get disk space and memory charts into the panel with their grouped info
 - [ ] Fix and refator --no-icons logic
 - [ ] Refactor and maybe rename --minimal flag to something like (--output "minimal | full")
-- [ ] Add different output format (eg. panels, trees, left panel)
+- [x] Add different output format (eg. panels, trees, left panel)
 - [ ] Plan to implement configuration (JSON File) for CLI app.
 - [ ] Add a colouring options (eg. rainbow, no color, color per group).
+
+## System info Output Formats
+- Panels: Display all system info inside panel.
+- Trees: Use trees widget to display system info using trees.
+- Left Panel: Get all sys info titles (or props) inside a left panel and the values to the right without any border or panel.
+- Plain: Display system info without using any visual widget, just as neofetch or fastfech do it right out of the box.
